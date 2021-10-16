f4a8462f0c73e166c46b154494cea03760a520c9
Laith Harb <laithharb@Laiths-iMac.local>
2021-10-16 18:00:30 -0400

    add paginationm

---
+++ 02-GraphQL-Design/typeDefs/06-Rule-Six.js
@@ -0,0 +1,32 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+`;
