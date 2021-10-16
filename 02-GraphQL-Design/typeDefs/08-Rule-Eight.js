4832fe2562354de3fc637b725f63bc29371acd3f
Laith Harb <laithharb@Laiths-iMac.local>
2021-10-16 19:07:50 -0400

    enum

---
+++ 02-GraphQL-Design/typeDefs/08-Rule-Eight.js
@@ -0,0 +1,37 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+`;
---
+++ 02-GraphQL-Design/typeDefs/09-Rule-Nine.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+
+  enum GroupFeatureFields {
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLUNDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
+  }
+`;
