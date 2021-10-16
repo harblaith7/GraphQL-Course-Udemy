c0c1d9303c125a1de6fb49ff256cd83b07c2c679
Laith Harb <laithharb@Laiths-iMac.local>
2021-10-16 19:56:38 -0400

    rule 11

---
+++ 02-GraphQL-Design/typeDefs/11-Rule-Eleven.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Mutation {
+    create
+    delete
+    update
+    publish
+    unpublish
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    hasCar(id: ID!): Boolean!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+`;
